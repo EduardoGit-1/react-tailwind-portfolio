e0622bc36c287b285740b27c7fbb4a1445b70e4b
Eduardo Gomes <eduardogomes9995@gmail.com>
2025-07-28 14:37:18 +0100

    added current work experience

--- src/context/data/timelineData.js
+++ src/context/data/timelineData.js
@@ -1,6 +1,7 @@
 
 import eumLogo from '../../assets/EEUMLOGO.png'
 import boschLogo from '../../assets/Logo-Bosch.png'
+import izLogo from '../../assets/impactzeroLogo.png'
 
 const timelineData = [
     {
@@ -37,6 +38,19 @@ const timelineData = [
             "First professional experience",
             "Worked with the Agile SCRUM methodology",
             "Development, implementation and comparative study of Deep Learning architectures for in-car violence detection using auditory data",
+        ],
+    },
+    {
+        title : "Backend and Mobile Developer",
+        subTitle: "Impactzero Software",
+        timeInterval: "May 2024 - Present",
+        img : izLogo,
+        list : [
+            "Designed and implemented a document parsing pipeline to extract and transmit data securely to multiple destinations, including legacy banking systems",
+            "Ensured compliance with banking standards by implementing secure and efficient data handling processes",
+            "Developed and maintained two Flutter-based mobile applications for restaurants and clients, ensuring a seamless user experience",
+            "Designed and implemented features for restaurant owners to manage menus, categories, custom dishes, and respective subscriptions",
+            "Optimized both apps' performance and user experience by collaborating with designers and backend teams"
         ],
     }
 ]
